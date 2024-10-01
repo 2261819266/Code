2fc8fa6a5ae36276633dc22f1b5f0379313fb634
yuxinhao <2261819266@qq.com>
2024-10-01 13:58:51 +0800

    Revert "master"
    This reverts commit a004b0b1fb702c1a41e6537b4a66a3338d868524.

--- 2024clyz/9.30/B.cpp
+++ 2024clyz/9.30/B.cpp
@@ -15,27 +15,8 @@ int pow(int x, int n) {
     return y % M;
 }
 
-int solve(int n) {
-    vector<vector<int>> f(n + 1, vector<int>(n + 1, 0));
-    f[0][0] = 1;
-    for (int i = 1; i <= n; i++) {
-        f[i][i] = (f[i - 1][i - 1] + f[i - 1][i]) % M;
-        for (int j = i + 1; j <= n; j++) {
-            if (i == 1) f[i][j] = 1;
-            else {
-                f[i][j] = (f[i - 1][j] + f[i][j - 1]) % M;
-            }
-        }
-    }
-    // for (auto i : f) {
-    //     for (int j : i) cout << j << " ";
-    //     cout << "\n";
-    // }
-    return f[n][n] % M;
-}
-
 signed main() {
     int n;
     cin >> n;
-    cout <<solve(n) << "\n";
+    cout << pow(2, n - 1);
 }
