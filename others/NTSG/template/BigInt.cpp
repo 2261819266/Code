6c94413efded75cfe9a37a86d97c9e7ac1071aac
yuxinhao <2261819266@qq.com>
2024-11-19 22:24:54 +0800

    master

--- others/NTSG/template/BigInt.cpp
+++ others/NTSG/template/BigInt.cpp
@@ -16,12 +16,12 @@ class BigInt {
     using ostream = std::ostream;
     using vector = std::vector<int>;
 
-    int operator[](int x) const { return a[x]; }
-    int &operator[](int x) { return a[x]; }
-    bool empty() const { return a.empty(); }
-    size_t size() const { return a.size(); }
-    BigInt update_zero() { while (!a.back()) a.pop_back(); return *this; }
-    BigInt(vector A, int SIG) : a(A), sig(SIG) {}
+    int operator[](int x) const { return data[x]; }
+    int &operator[](int x) { return data[x]; }
+    bool empty() const { return data.empty(); }
+    size_t size() const { return data.size(); }
+    BigInt update_zero() { while (!data.back()) data.pop_back(); return *this; }
+    BigInt(vector A, int SIG) : data(A), sig(SIG) {}
 
     template<typename T> string to_string(const T &x) const { return std::to_string(x); }
     int stoi(const string &s) { return stoi(s, 0, s.size()); }
@@ -31,15 +31,15 @@ class BigInt {
     template<typename T> BigInt operator<<(T x) const {
         if (!*this) return 0;
         BigInt ans(vector(x, 0), sig);
-        for (const int &i : a) ans.a.push_back(i);
+        for (const int &i : data) ans.data.push_back(i);
         return ans.update_zero();
     }
 
     template<typename T> BigInt &operator>>=(T x) { return *this = *this >> x; }
-    template<typename T> BigInt operator>>(T x) const { return x < size() ? (BigInt){vector(a.begin() + x, a.end()), sig}.update_zero() : (BigInt)0; }
+    template<typename T> BigInt operator>>(T x) const { return (size_t)x < size() ? (BigInt){vector(data.begin() + x, data.end()), sig}.update_zero() : (BigInt)0; }
 
   public:
-    BigInt(void) : a(), sig(false) {}
+    BigInt(void) : data(), sig(false) {}
     template<typename T> BigInt(const T &x) { *this = x; }
     BigInt(const string &x) { *this = x; }
     BigInt(const char *x) { *this = x; }
@@ -49,14 +49,14 @@ class BigInt {
         T x = X;
         if ((sig = x < 0)) x = -x;
         do {
-            a.push_back(x % MOD);
+            data.push_back(x % MOD);
         } while (x /= MOD);
         return *this;
     }
     BigInt &operator=(const string &_s) {
         string s = _s;
         if (_s.front() == '-') sig = 1, s = string(_s, 1);
-        a.clear();
+        data.clear();
         int len = _s.size();
         int k;
         for (k = 0; k < len && s[k] == '0'; k++);
@@ -65,9 +65,9 @@ class BigInt {
         if (!len) return clear();
         int y = len % BASE;
         if (!y) y = BASE;
-        a.push_back(stoi(s, 0, y));
-        for (int x = y; x < len; x += BASE) a.push_back(stoi(s, x, x + BASE));
-        std::reverse(a.begin(), a.end());
+        data.push_back(stoi(s, 0, y));
+        for (int x = y; x < len; x += BASE) data.push_back(stoi(s, x, x + BASE));
+        std::reverse(data.begin(), data.end());
         return *this;
     }
     BigInt &operator=(const char *s) { return *this = s; }
@@ -80,55 +80,55 @@ class BigInt {
         return in;
     }
 
-    BigInt &clear() { sig = 0, a.clear(); return *this; }
+    BigInt &clear() { sig = 0, data.clear(); return *this; }
 
     template<typename T>
     operator T() const {
         T x = 0;
-        for (const T &i : a) (x *= MOD) += i;
+        for (const int &i : data) (x *= MOD) += i;
         return x * (1 - 2 * sig);
     }
     operator string() const {
-        if (a.empty()) return "0";
+        if (data.empty()) return "0";
         string S;
         if (sig) S = "-";
-        int len = a.size();
+        int len = data.size();
         for(int i = len - 1; i >= 0; i--) {
-            string s = to_string(a[i]);
+            string s = to_string(data[i]);
             for (int j = s.size(); j < BASE && i < len - 1; j++) S += '0';
             S += s;
         }
         return S;
     }
-    operator bool() const { return !a.empty(); }
-    bool operator!() const { return a.empty(); }
+    operator bool() const { return !data.empty(); }
+    bool operator!() const { return data.empty(); }
 
     bool operator<(const BigInt &b) const {
         if (*this == b) return false;
         bool ans;
-        if (a.empty() && b.empty()) return false;
-        if (a.empty()) return !b.sig;
+        if (data.empty() && b.empty()) return false;
+        if (data.empty()) return !b.sig;
         if (b.empty()) return sig;
         if (sig != b.sig && (!!*this || !!b)) return b.sig < sig;
-        else if (a.size() != b.size()) ans = a.size() < b.size();
+        else if (data.size() != b.size()) ans = data.size() < b.size();
         else {
-            int n = a.size();
+            int n = data.size();
             for (int i = n - 1; i >= 0; i--) {
-                if (a[i] != b.a[i]) {
-                    ans = a[i] < b.a[i];
+                if (data[i] != b.data[i]) {
+                    ans = data[i] < b.data[i];
                     break;
                 }
             }
         }
         return ans ^ sig;
     }
-    bool operator==(const BigInt &b) const { return sig == b.sig && a == b.a; }
+    bool operator==(const BigInt &b) const { return sig == b.sig && data == b.data; }
     bool operator!=(const BigInt &b) const { return !(*this == b); }
     bool operator<=(const BigInt &b) const { return !(b < *this); }
     bool operator>=(const BigInt &b) const { return !(*this < b); }
     bool operator>(const BigInt &b) const { return b < *this; }
 
-    BigInt operator-() const { return {a, !*this ? 0 : !sig}; }
+    BigInt operator-() const { return {data, !*this ? 0 : !sig}; }
 
     BigInt abs() const { return sig ? -*this : *this; }
 
@@ -140,8 +140,8 @@ class BigInt {
         if (sig != b.sig) return *this - (-b);
         if (*this < b) return b + *this;
         BigInt ans(*this);
-        int n = a.size(), m = b.size();
-        ans.a.push_back(0);
+        int n = data.size(), m = b.size();
+        ans.data.push_back(0);
         for (int i = 0, d = 0; i <= n; i++) ans [i] -= MOD * (d = (ans[i] += (i < m ? b[i] : 0) + d) / MOD);
         return ans.update_zero();
     }
@@ -154,7 +154,7 @@ class BigInt {
         if (sig != b.sig) return *this + (-b);
         if (abs() < b.abs()) return -(b - *this);
         BigInt ans = *this;
-        int n = a.size(), m = b.size();
+        int n = data.size(), m = b.size();
         for (int i = 0, d = 1; i < n; i++) ans[i] -= MOD * (d = (ans[i] += MOD + d - 1 - (i < m ? b[i] : 0)) / MOD);
         return ans.update_zero();
     }
@@ -166,7 +166,7 @@ class BigInt {
         if (b < 0) return -(*this * (-b)); 
         int n = size();
         BigInt ans = *this;
-        ans.a.resize(n + 2);
+        ans.data.resize(n + 2);
         for (int i = 0, d = 0; i < n || d; i++) 
             ans[i] -= (d = ((ans[i] *= b) += d) / MOD) * MOD; 
         return ans.update_zero();
@@ -179,22 +179,47 @@ class BigInt {
         ans.sig = sig ^ b.sig;
         return ans.update_zero();
     }
-
+    
+    template<typename T> BigInt operator/(const T &b) const { return *this / (BigInt)b; }
+    template<typename T> BigInt &operator/=(const T &b) { return *this = *this / b; }
     BigInt operator/(const signed &b) const {
         if (!b) { std::cerr << "MATH ERROR!\n"; exit(-1); }
         if (!*this) return 0;
         if (b < 0) return -*this / (-b);
         BigInt ans = 0;
         int n = size();
-        for (int i = n - 1, d = 0; i >= 0; i--) (ans <<= 1) += (a[i] + d * MOD) / b, d = (a[i] + d * MOD) % b;
+        for (int i = n - 1, d = 0; i >= 0; i--) (ans <<= 1) += (data[i] + d * MOD) / b, d = (data[i] + d * MOD) % b;
         ans.sig = sig;
         return ans.update_zero();
 	}
+    BigInt operator/(const BigInt &b) const {
+        if (!b) { std::cerr << "MATH ERROR!\n"; exit(-1); }
+        if (!*this) return 0;
+        // if (b.size() <= 1) return *this / (signed)b;
+        if (sig & b.sig) return (-*this) / (-b);
+        if (sig ^ b.sig) return -(abs() / b.abs());
+        BigInt ans, d = b, a = *this;
+        int e = (size() - b.size() + 1);
+        ans.data.assign(e + 1, 0);
+        d <<= e;
+        while (true) {
+            while (a >= d) {
+                int x = a.data.back() / (d.data.back() + 1);
+                a -= d * x;
+                ans[e] += x;
+            }
+            while (a >= b * d) a -= d;
+            if (a < b) break;
+            e--;
+            d >>= 1;
+        }
+        return ans.update_zero();
+    }
 
   private:
     static const int BASE = 8;
     static const int MOD = 1e8;
-    vector a;
+    vector data;
     bool sig;
 
 };
