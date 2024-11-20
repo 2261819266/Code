22ab5155eff649ef324fd6ed8f0636da005ac5be
yuxinhao <2261819266@qq.com>
2024-11-20 22:05:17 +0800

    master

--- luogu/P1074.cpp
+++ luogu/P1074.cpp
@@ -110,7 +110,8 @@ bool checkChunk(int x, int y) {
 
 bool check(int x, int y) { return checkX(x, y) & checkY(x, y) & checkChunk(x, y); }
 
-PII getnext(int x, int y) { return {(x + 1) % n, y + (x == n - 1)}; }
+
+
 
 int ans = -1;
 
@@ -124,18 +125,43 @@ void update() {
     if (y > ans) ans = y;
 }
 
+vector<int> get(int x, int y) {
+    vector<int> vis(n + 1, 0);
+    for (int i = 0; i < n; i++) vis[a[x][i]] = vis[a[i][y]] = true;
+    x = x / 3 * 3;
+    y = y / 3 * 3;
+    for (int i = x; i < x + 3; i++) {
+        for (int j = y; j < y + 3; j++) {
+            vis[a[i][j]]++;
+        }
+    }
+    return vis;
+}
+
+int X, Y, vis = 0, Z;
+int T[9][9], I;
+vector<PII> A;
+map<PII, int> mp;
+
+PII getnext(int x, int y) { return A[mp[{x, y}] + 1]; }
+
 void dfs(int x = 0, int y = 0) {
     PII nxt = getnext(x, y);
-    if (y == n) update();
-    else if (!a[x][y]) {
-        for (int i = 1; i <= n; i++) {
-            a[x][y] = i;
-            if (check(x, y)) {
-                dfs(nxt.first, nxt.second);
+    if (y == -1 && x == -1) update();
+    else {
+        // vis = true;
+        
+        vector<int> v = get(x, y);
+        if (!a[x][y]) {
+            for (int i = 1; i <= n; i++) {
+                a[x][y] = i;
+                if (!v[i]) {
+                    dfs(nxt.first, nxt.second);
+                }
             }
-        }
         a[x][y] = 0;
     } else dfs(nxt.first, nxt.second);
+    }
 } 
 
 void main() {
@@ -148,8 +174,55 @@ void main() {
             }
         }
     }
-    dfs();
+    while (true) {
+        int k = 0;
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (a[i][j]) continue;
+                int t = 0, s;
+                vector<int> v = get(i, j);
+                for (int l = 1; l <= n; l++) {
+                    t += !!v[l];
+                    if (!v[l]) s = l;
+                }
+                if (t == n - 1) {
+                    k++;
+                    a[i][j] = s;
+                    // cerr << i << spc << j << spc << s << endl;
+                }
+                if (t > Z && !a[i][j] && t < n - 1) X = i, Y = j, Z = t;
+            }
+        }
+        if (!k) break;
+    }
+    
+    // for (const auto &i : a) {
+    //     for (auto j : i) cout << j << spc;
+    //     cout << endl;
+    // }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            vector<int> v = get(i, j);
+            int t = 0;
+            for (int l = 1; l <= n; l++) t += !v[l];
+            T[i][j] = t;
+        }
+    }
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j < n; j++) {
+            for (int k = 0; k < n; k++) {
+                if (T[j][k] == i) mp[{j, k}] = A.size(), A.push_back({j, k});
+            }
+        }
+    }
+
+    A.push_back({-1, -1});
+    
+    dfs(X, Y);
     cout << ans << endl;
+    cerr << clock();
 }
 }
 
